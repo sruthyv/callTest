a21c3068d5fb465003e41ffd4a4e7d17219a7556
sruthyv <vsruthys@gmail.com>
2017-09-13 16:07:59 +0530

    check phone number validity

--- call_check.rb
+++ call_check.rb
@@ -1,28 +1,31 @@
 class CallCheck
   
   def method_missing(method_name, *args, &block)
-     @phoneString = method_name[/\d+/].to_s 
-     # puts @phoneString
-     @lengthOfNum = @phoneString.length
-     # puts @lengthOfNum
-     if @lengthOfNum == 11
-        if @phoneString.start_with? '0484' 
-           return "valid number"
-        else
-           return "invalid number"
-        end
-        
-     elsif @lengthOfNum == 10
-        if (@phoneString.start_with? '7')||(@phoneString.start_with? '8' )|| 
-                                              (@phoneString.start_with? '9')
-           return "valid number"
-        else
-           return  "invalid number"
-        end
+    @phoneString = method_name[/\d+/].to_s
+    @phoneNumber = method_name[5..method_name.length] 
+    @lengthOfNum = @phoneString.length
+    if @phoneString != @phoneNumber
+        return "invalid number"
     else
-        return "invalid number"        
-    end            
-   end
+        if @lengthOfNum == 11
+          if @phoneString.start_with? '0484' 
+             return "valid number"
+          else
+             return "invalid number"
+          end
+        
+        elsif @lengthOfNum == 10
+          if (@phoneString.start_with? '7')||(@phoneString.start_with? '8' )|| 
+                                              (@phoneString.start_with? '9')
+             return "valid number"
+          else
+             return  "invalid number"
+          end
+          else
+            return "invalid number"        
+          end
+        end            
+    end
 end
 
 
